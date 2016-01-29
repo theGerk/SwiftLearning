dc53d6178a2eefe77b0e638b758d326ae76fb1b3
= <benji@altmansoftwaredesign.com>
2016-01-29 15:48:53 -0600

    Initial commit

---
+++ Learning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learning
+//
+//  Created by Benjamin D Altman on 1/29/16.
+//  Copyright (c) 2016 Benjamin D Altman. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
